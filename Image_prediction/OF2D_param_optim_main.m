3d1beb7ba9bc8adc58a9e6f179bd35f7a9bca1a8
Michel Pohl <michel.pohl.20.08@gmail.com>
2023-02-19 17:59:44 +0000

    Resolved bug corresponding to different sigma_subspl values selected when only one layer is optimal during DVF optimization

--- Image_prediction/OF2D_param_optim_main.m
+++ Image_prediction/OF2D_param_optim_main.m
@@ -42,13 +42,13 @@ path_par.input_im_dir_suffix_tab = [
     %string('10. sq sl014 sag Xcs=125')
     %string('11. sq sl014 sag Xcs=150')
     string('2020-11-10_KS81_Nav_Pur_1')
-    string('2020-11-12_QN76_Nav_Pur_1')
-    string('2020-11-17_CS31_Nav_Pur_2')
-    string('2020-11-17_JY02_Nav_Pur_2')
-    string('2020-11-23_ON65_Nav_Pur_2')
-    string('2020-11-23_PS11_Nav_Pur_1')
-    string('2020-11-25_II29_Nav_Pur_1')
-    string('2020-11-26_NE38_Nav_Pur_1')
+    %string('2020-11-12_QN76_Nav_Pur_1')
+    %string('2020-11-17_CS31_Nav_Pur_2')
+    %string('2020-11-17_JY02_Nav_Pur_2')
+    %string('2020-11-23_ON65_Nav_Pur_2')
+    %string('2020-11-23_PS11_Nav_Pur_1')
+    %string('2020-11-25_II29_Nav_Pur_1')
+    %string('2020-11-26_NE38_Nav_Pur_1')
     ];
 
 % Hyper-parameter grid search :
@@ -77,10 +77,10 @@ for im_seq_idx = 1:nb_seq
 
     % Image parameters
     im_par = load_im_param(path_par);
-    im_par.nb_im = 170; 
+    im_par.nb_im = 2; 
         % ETH Zürich: optimization using the first 90 images of the sequence (arbitrary)
         % Magdeburg University: 170 images
-        % software testing: 3 images (2 will not work)
+        % software testing: 2 images
 
     %% EVALUATION FOR EACH HYPER-PARAMETER SET IN THE GRID
     
