813b1f2b49edb214ac49b19692264d2781e1e59e
Michel Pohl <michel.pohl.20.08@gmail.com>
2024-03-25 16:23:58 +0900

    Latest image displayed as background when saving OF display by default.

--- Image_prediction/6. Auxiliary functions (loading, saving files and parameters)/save2DOFjpg.m
+++ Image_prediction/6. Auxiliary functions (loading, saving files and parameters)/save2DOFjpg.m
@@ -1,4 +1,4 @@
-function save2DOFjpg(beh_par, path_par, disp_par, OF_par, im_par)
+function save2DOFjpg(beh_par, path_par, disp_par, OF_par, im_par, is_background_first_im)
 % Loads the optical flow previously calculated by compute_2Dof and displays it on top of the initial image at t=1.
 % The DVF images are saved in the folder path_par.im_par_filename if beh_par.SAVE_OF_JPG is set to true
 %
@@ -7,19 +7,29 @@ function save2DOFjpg(beh_par, path_par, disp_par, OF_par, im_par)
 % Version : v2.0
 % License : 3-clause BSD License
 
-    
+    if nargin < 6
+        is_background_first_im = false;
+    end
+
+    enhance_contrast = false;    
+
     % loading image at time t=1
-    im_t1 = enhance_2Dim(load_crop_filter2D(1, beh_par.CROP_FOR_DISP_SAVE, false, 0, im_par, path_par.input_im_dir), true);    
+    background_im = enhance_2Dim(load_crop_filter2D(1, beh_par.CROP_FOR_DISP_SAVE, false, 0, im_par, path_par.input_im_dir), enhance_contrast);    
         % the image is cropped if we select a specific region where the optical flow should be displayed (beh_par.CROP_FOR_DISPLAY = 1)
         % The image is not filtered because it is used to display the optical flow here (FILTER = false)
     
-    G = return_mask_for_DVF_display(im_t1, disp_par);
+    G = return_mask_for_DVF_display(background_im, disp_par);
 
     for t=2:im_par.nb_im
         OF_t_filename = write_2DOF_t_mat_filename( OF_par, path_par, t );
         load(OF_t_filename, 'u_t');
         OF_t_png_filename = write_2DOF_t_png_filename(beh_par, OF_par, path_par, t);
-        display_save_DVF(u_t, im_t1, G, OF_t_png_filename, im_par, beh_par, disp_par, beh_par.SAVE_OF_JPG);    
+
+        if ~is_background_first_im
+            background_im = enhance_2Dim(load_crop_filter2D(t, beh_par.CROP_FOR_DISP_SAVE, false, 0, im_par, path_par.input_im_dir), enhance_contrast);    
+        end
+                
+        display_save_DVF(u_t, background_im, G, OF_t_png_filename, im_par, beh_par, disp_par, beh_par.SAVE_OF_JPG);    
     end
     
 end
