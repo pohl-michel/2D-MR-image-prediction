eb0e19b335a4b101f100e6df935ffc93a37e2965
Michel Pohl <michel.pohl.20.08@gmail.com>
2024-04-08 23:24:55 +0900

    Removed calculation of principal components in eval_im_pred_best_par as they were already computed in select_nb_pca_cp

--- Image_prediction/3. Image warping and prediction/eval_im_pred_best_par.m
+++ Image_prediction/3. Image warping and prediction/eval_im_pred_best_par.m
@@ -13,6 +13,10 @@ function eval_im_pred_best_par(eval_results, best_pred_par_struct, best_pca_cp_t
 
     beh_par.SAVE_WARPED_IM = false; % because display takes time and this function needs to run fast 
     beh_par.SAVE_PREDICTION_PLOT = false; % idem
+    beh_par.EVAL_INIT_OF_WARP = true;            
+    beh_par.IM_PREDICTION = true;
+    beh_par.EVAL_PCA_RECONSTRUCT = true;  
+    beh_par.SAVE_PRED_RESULTS = true; % so that eval_of_warp_cor can correctly load the predicted DVF    
 
     pred_par = load_pred_par(path_par, pred_meth); %we need to know tmax_pred corresponding to the test set and the prediction method (in particular)
         % in the case of linear regression, the value of pred_par.tmax_training is already modified inside the function "load_pred_par"
@@ -29,30 +33,13 @@ function eval_im_pred_best_par(eval_results, best_pred_par_struct, best_pca_cp_t
     my_empty_struct = struct();
     eval_results = eval_of_warp_corr(dvf_type, im_par, OF_par, path_par, warp_par, pred_par, br_model_par, disp_par, beh_par, eval_results, my_empty_struct);                   
 
-    beh_par.EVAL_INIT_OF_WARP = true;            
-    beh_par.IM_PREDICTION = true;
-    beh_par.EVAL_PCA_RECONSTRUCT = true;  
-    beh_par.SAVE_PRED_RESULTS = true; % so that eval_of_warp_cor can correctly load the predicted DVF
-
-    eval_results_cell = cell(1, hppars.nb_hrz_val);
-
-    fprintf("Computation of the PCA components with the optimal number of components for each horizon tested \n");
-    for hrz_idx = 1:hppars.nb_hrz_val % for loop otherwise different threads may attempt to write the same file, leading to a "corrupt file" error
-        pred_par.horizon = hppars.horizon_tab(hrz_idx);
-        br_model_par_h.nb_pca_cp = best_pca_cp_tab(hrz_idx);
-        for hppar_idx = 1:hppars.nb_additional_params
-            pred_par.(hppars.other(hppar_idx).name) = best_pred_par_struct(br_model_par_h.nb_pca_cp).other_hyppar_tab(hrz_idx, hppar_idx);
-        end                
-        [W, F, Xmean, eval_results_cell{1, hrz_idx}] = compute_PCA_of_DVF(beh_par, disp_par, OF_par, im_par, path_par, pred_par, br_model_par_h, eval_results); 
-    end
-
     parfor hrz_idx = 1:hppars.nb_hrz_val
 
         pred_par_h = pred_par;
         br_model_par_h = br_model_par;
         path_par_h = path_par;
         warp_par_h = warp_par;
-        eval_results_h = eval_results_cell{1, hrz_idx};
+        eval_results_h = eval_results;
 
         pred_par_h.horizon = hppars.horizon_tab(hrz_idx);
         br_model_par_h.nb_pca_cp_max = br_model_par.nb_pca_cp; % used in write_im_pred_log_file below
