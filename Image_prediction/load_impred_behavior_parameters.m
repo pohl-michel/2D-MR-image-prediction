43d8253c24a798a8da02b373adac154e3a4bbf40
Michel Pohl <michel.pohl.20.08@gmail.com>
2024-10-06 15:08:28 +0100

    Improved documentation for load_impred_behavior_parameters.m

--- Image_prediction/load_impred_behavior_parameters.m
+++ Image_prediction/load_impred_behavior_parameters.m
@@ -1,67 +1,47 @@
 function [ beh_par ] = load_impred_behavior_parameters()
-% The structure beh_par contains important information about the behavior of the whole algorithm,
-% and its fields should be set manually.
-% 
-% To do: continue documentation for each field
+% LOAD_IMPRED_BEHAVIOR_PARAMETERS Loads and returns the behavior parameters for the image prediction algorithm.
+%
+% INPUTS:
+%   (None)
+%
+% OUTPUTS:
+% - beh_par (struct): Structure containing the behavior parameters for the image prediction algorithm
 %
 % Author : Pohl Michel
-% Date : Sept 18th, 2022
-% Version : v1.0
 % License : 3-clause BSD License
 
 
-%% IMPORTANT PARAMETERS
+%% USER-ADJUSTABLE PARAMETERS
 
-beh_par.SAVE_ROI_DISPLAY = false;
-    % For saving the initial image of the sequence with the region of interest (ROI) in a red rectangle 
-beh_par.SAVE_ORG_IM_SQ = false;
-    % For saving jpg images of the original image sequence that we try to predict
-beh_par.SAVE_MEAN_IMAGE = false;
-    % For saving the average image (over every time step) of the test set
+beh_par.SAVE_ROI_DISPLAY = false;  % Save the first image in the sequence with the region of interest (ROI) in a red rectangle
+beh_par.SAVE_ORG_IM_SQ = false;    % Save jpg images of the original image sequence
+beh_par.SAVE_MEAN_IMAGE = false;   % Save the average image of the test set
 
-beh_par.COMPUTE_OPTICAL_FLOW = false;
-    % For computing the optical flow (OF) / deformation vector field (DVF) at every time step and saving the results as mat files
-beh_par.SAVE_OF_JPG = false;
-    % For saving DVF images
-beh_par.EVAL_INIT_OF_WARP = false;
-    % For computing statistics describing deformable image registration (DIR) accuracy of the test set
-beh_par.SAVE_INIT_OF_WARP_JPG = false;
-    % For saving the image at t=1 warped by the initial DVF/OF at time t for each time step t of the test set
+beh_par.COMPUTE_OPTICAL_FLOW = false;   % Compute and save (as .mat files) optical flow at every time step
+beh_par.SAVE_OF_JPG = false;            % Save optical flow images as jpg files
+beh_par.EVAL_INIT_OF_WARP = false;      % Evaluate deformable image registration (DIR) accuracy of the test set
+beh_par.SAVE_INIT_OF_WARP_JPG = false;  % Save the first image warped by the initial deformation vector field (DVF) at every time step of the test set
 
-beh_par.OPTIMIZE_NB_PCA_CP = false;
-    % For optimizing the number of PCA components for prediction using hyper-parameter grid search
-beh_par.REGISTRATION_ERROR_CV = false;
-    % For optimizing the number of PCA components based on the DVF registration NRMSE rather than cross-correlation between the initial image and the warped images 
+beh_par.OPTIMIZE_NB_PCA_CP = false;     % Optimize number of PCA components via grid search
+beh_par.REGISTRATION_ERROR_CV = false;  % Optimize PCA components based on registration nRMSE with predicted DVF (instead of cross-correlation between the original and warped images)
 
-beh_par.PCA_OF_DVF = false;
-    % For computing principal component analysis (PCA) from the DVF data
-beh_par.SAVE_PCA_CP_WEIGHTS_JPG = false;
-    % For saving jpg images of the principal components (the 2D principal deformation vectors)
-beh_par.EVAL_PCA_RECONSTRUCT = false;
-    % For evaluating the quality of the DVFs reconstructed with a few principal components by warping the initial image at t=1 by the DVF at time t for each
-    % time step t of the test set
-beh_par.SAVE_PCA_RECONSTR_JPG = false;
-    % For saving the image at t=1 warped by the DVF/OF reconstructed from PCA at time t for each time step t of the test set
+beh_par.PCA_OF_DVF = true;                % Compute PCA from the DVF data
+beh_par.SAVE_PCA_CP_WEIGHTS_JPG = false;  % Save 2D principal deformation vectors fields (PCA components) as jpg images
+beh_par.EVAL_PCA_RECONSTRUCT = false;     % Evaluate quality of DVF reconstructed using PCA (by warping the first image at t=1 by the DVF at time t when t corresponds to the test set)
+beh_par.SAVE_PCA_RECONSTR_JPG = false;    % Save the image at t=1 warped by the DVF reconstructed from PCA at time t for each time step t of the test set
 
-beh_par.TRAIN_EVAL_PREDICTOR = false;
-    % For training and evaluating the prediction/forecast of the PCA weights
-beh_par.SAVE_PRED_RESULTS = false;
-    % For saving a mat file containing the prediction results (predicted PCA weights, loss function, and prediction time)
+beh_par.TRAIN_EVAL_PREDICTOR = false;  % Train the PCA weight forecasting algorithm and evaluate it
+beh_par.SAVE_PRED_RESULTS = false;     % Save a .mat file containing the prediction results (predicted PCA weights, loss function, and time performance)
 
-beh_par.NO_PRED_AT_ALL = true; 
-    % The original images are used instead of predicted images as a base case for performance evaluation
-beh_par.IM_PREDICTION = true; 
-    % Performing image prediction
-beh_par.SAVE_PRED_IM = false; 
-    % Saving the predicted images and the thermal difference images
+beh_par.NO_PRED_AT_ALL = false;   % Use original images instead of predictions for performance evaluation
+beh_par.IM_PREDICTION = true;     % Perform image prediction
+beh_par.SAVE_PRED_IM = true;      % Save the predicted images and thermal difference images
 
-beh_par.CROP_FOR_DISP_SAVE = false;
-    % For saving the deformation vector field images, predicted images, or error images only in the ROI, i.e. the area specified by x_m, x_M, y_m, y_M
-beh_par.EVALUATE_IN_ROI = true;
-    % if EVALUATE_IN_ROI is set to true, the prediction errors are calculated using only the pixels in the region of interest (ROI)
+beh_par.CROP_FOR_DISP_SAVE = false;  % Save the deformation vector field images, predicted images, or error images only in the ROI
+beh_par.EVALUATE_IN_ROI = false;     % Evaluate prediction errors only within the ROI
 
 
-%% OTHER PARAMETERS (do not modify)
+%% PARAMETERS AUTOMATICALLY SET
 beh_par.SAVE_WARPED_IM = beh_par.SAVE_INIT_OF_WARP_JPG || beh_par.SAVE_PCA_RECONSTR_JPG || beh_par.SAVE_PRED_IM;
 beh_par.SAVE_PREDICTION_PLOT = true;
 beh_par.EVALUATE_PREDICTION = true;
