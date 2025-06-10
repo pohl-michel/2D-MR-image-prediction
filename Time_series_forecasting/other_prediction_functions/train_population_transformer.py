b994cebeded195456477950d9d858724ffcf957a
Michel WSL <michel.pohl.20.08@gmail.com>
2025-06-10 20:04:44 +0100

    Added config file loading step to population transformer training app

--- Time_series_forecasting/other_prediction_functions/train_population_transformer.py
+++ Time_series_forecasting/other_prediction_functions/train_population_transformer.py
@@ -1,13 +1,28 @@
 import argparse
+import json
+import os
 import sys
 
 from . import transformers_forecasting as tf
 
+CONFIG_DIR = "other_prediction_functions"
+
 
 def main(argv):
     parser = argparse.ArgumentParser(description="Train population transformer model")
+    parser.add_argument(
+        "--config",
+        help="path to JSON config file",
+        default="pop_transformer_training_config_test.json",
+        nargs=1,
+        type=str,
+    )
     args = parser.parse_args(argv)
     print("✅ Training population transformer model...")
+    print(f"Using config file: {args.config}")
+
+    with open(os.path.join(CONFIG_DIR, args.config), "r") as f:
+        config = json.load(f)
 
     return 0
 
