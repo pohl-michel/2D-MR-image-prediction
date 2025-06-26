73c3d8a4bb798742b0deb1421bc3b46ae9f6c8c7
mpohl <michel.pohl.20.08@gmail.com>
2025-06-26 09:29:04 +0100

    Fix: Modified how python path is set inside Matlab so that it does not depend on the name of the project directory

--- Time_series_forecasting/other_prediction_functions/get_python_transformers_module_dir.m
+++ Time_series_forecasting/other_prediction_functions/get_python_transformers_module_dir.m
@@ -1,10 +1,10 @@
 function moduleDir = get_python_transformers_module_dir()
 
     cwd = pwd;
-    if endsWith(cwd, 'MRI_prediction')
-        moduleDir = fullfile(cwd, 'Time_series_forecasting', 'other_prediction_functions');
-    else
+    if endsWith(cwd, 'Time_series_forecasting')
         moduleDir = fullfile(cwd, 'other_prediction_functions');
+    else % we are calling from the root project (2D-MR-image-prediction or other name if it was renamed)
+        moduleDir = fullfile(cwd, 'Time_series_forecasting', 'other_prediction_functions');
     end
 
 end
